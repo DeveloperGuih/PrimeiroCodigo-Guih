559535cb2a86fd8cacbdf8155aba4c773e2b2dc7
Guih <bmoraeskkj@gmail.com>
2026-02-02 15:18:50 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Código do Guih</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php
+            $mensagem = "Olá, este é um exemplo de código PHP!";
+            $name = "Guih";
+            echo "<h1>$mensagem</h1>";
+            echo "<h1>Feito por: $name</h1>";
+        ?>
+</body>
+
+</html> 
