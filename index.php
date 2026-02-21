e7643c63855cf494685a8373c35f1ae752bd1ad6
Guih <148808554+DeveloperGuih@users.noreply.github.com>
2026-02-21 18:45:43 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Código do Guih</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 
 <body>
     <div class="container">
+        <img src="assets/monagato.png" alt="monagato">
         <?php
             $mensagem = "Olá, este é um exemplo de código PHP!";
             $name = "Guih";
